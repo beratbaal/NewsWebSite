31a295c69fae37f2f83566f06614dcda46863c98
Berat <64924845+beratbaal@users.noreply.github.com>
2022-05-07 20:21:30 +0300

    API Added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,51 @@
 import './App.css';
 import { NavBar } from "./Views/NavBar";
+import { Slider } from './Views/Slider';
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 function App() {
 
-  function MainPage() {
+  const [topNewsList, setTopNewsList] = useState(["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",]);
+
+ 
+  useEffect(() => {
+    Apifetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=bdff393e72104f87a7accf76c2b070a6', setTopNewsList)
     
-    return(
+
+
+  }, []);
+  function Apifetch(url, setList) {
+    fetch(url
+    ).then(response => {
+      return response.json();
+    }).then(data => {
+
+      const news = data["results"];
+      setList(news)
+      console.log(news)
+    });
+  }
+
+  function MainPage() {
+
+    return (
       <div>
-        <NavBar/>
+        <NavBar />
+        <Slider news={topNewsList.slice(10)}/>
       </div>
     );
-   
+
   }
-   return(
+  return (
     <BrowserRouter>
       <Routes>
-        <Route index path="/" element={<MainPage/>}/>
+        <Route index path="/" element={<MainPage />} />
       </Routes>
     </BrowserRouter>
-   );
- 
+  );
+
 }
 
-  
+
 export default App;
---
+++ src/Components/SliderComp.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "../CSS/Slider.css";
+
+
+function SliderComp({
+    newstitle,newsdescreption,newsposter
+}){
+  
+    return(
+        <div className="item">
+       
+        <div className="carousel-caption">
+          <h3 className="newstitlecustom">{newstitle}</h3>
+          <p className="newsdescreption">{newsdescreption}</p>
+    
+     
+        </div>
+      </div>
+    );
+}
+
+export default SliderComp
