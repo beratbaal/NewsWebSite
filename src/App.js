dbbb0b37833f41dce96047c78f20c5ea161c5ea2
Berat <64924845+beratbaal@users.noreply.github.com>
2022-05-08 02:35:15 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,27 +6,6 @@ import { useEffect, useState } from 'react';
 
 function App() {
 
-  const [topNewsList, setTopNewsList] = useState(["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",]);
-
- 
-  useEffect(() => {
-    Apifetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=bdff393e72104f87a7accf76c2b070a6', setTopNewsList)
-    
-
-
-  }, []);
-  function Apifetch(url, setList) {
-    fetch(url
-    ).then(response => {
-      return response.json();
-    }).then(data => {
-
-      const news = data["results"];
-      setList(news)
-      console.log(news)
-    });
-  }
-
   function MainPage() {
 
     return (
